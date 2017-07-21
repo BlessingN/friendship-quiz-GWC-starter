efbd6ea36a4a8fc8c7f28ff9feb7680f807631eb
Blessing Ntekume <blessing@Blessings-MacBook-Air.local>
2017-07-21 14:44:00 -0700

    Uploading finished project

--- friendship-quiz.js
+++ friendship-quiz.js
@@ -1,5 +1,5 @@
 // questions to be randomly asked on page load
-var questionsList = ["What are your/your friend's favorite foods?", "What are your/your friend's favorite animals?"];
+var questionsList = ["What are your/your friend's favorite foods?", "What are your/your friend's favorite animals?", ];
 
 
 //randomly chooses a question to display
@@ -16,10 +16,25 @@ var answerList = []; //initialize answerList to be empty
 
 //adds user-submitted answer to answerList
 function submitAnswer() {
-
+  var ans = document.getElementById("answerInput").value;
+  answerList.push(ans);        //stores submitted answers into the answerList
+  console.log(answerList);     //logs answers into console
 }
+
 
 //checks if user-submitted guess is in answerList
 function checkGuess() {
+    found = false
+    var guess = document.getElementById("guessInput").value;
+    console.log(guess)        // logs guesses into the console
 
+for (var i = 0; i < answerList.length; i++) {   //checks each answer in console
+  if (guess == answerList[i]) {
+    alert("Correctomundo");
+    found = true;
+  }
+}
+  if (found == false) {       //If the guess does not match the answer, popup "Nah Bruh"
+      alert("Nah bruh");
+    }
 }
